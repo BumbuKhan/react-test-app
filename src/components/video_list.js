ee5950e8088d24ecbfa11ab0adc6bd109706ae27
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-09-30 12:34:15 +0400

    App has been created

---
+++ src/components/video_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoItem from './video_list_item';
+
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => <VideoItem onVideoSelect={props.onVideoSelect} key={video.etag} video={video}/>);
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    )
+};
+
+export default VideoList;
